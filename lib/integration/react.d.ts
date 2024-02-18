839ab718f5bb49969fcb2f64db7b19d46ac799f0
Massimo Salvetto <26168376+MassimoSalvetto@users.noreply.github.com>
2024-02-17 22:47:29 -0600

    chore: build

---
+++ lib/integration/react.d.ts
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+import type { Persistor } from '../types';
+declare type Props = {
+    onBeforeLift?: () => void;
+    children: ReactNode | ((state: boolean) => ReactNode);
+    loading: ReactNode;
+    persistor: Persistor;
+};
+declare type State = {
+    bootstrapped: boolean;
+};
+export declare class PersistGate extends React.PureComponent<Props, State> {
+    static defaultProps: {
+        children: null;
+        loading: null;
+    };
+    state: {
+        bootstrapped: boolean;
+    };
+    _unsubscribe?: () => void;
+    componentDidMount(): void;
+    handlePersistorState: () => void;
+    componentWillUnmount(): void;
+    render(): ReactNode;
+}
+export {};
